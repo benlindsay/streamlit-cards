78571aa2db5a82fb81a71997184683375a9cddff
Ben Lindsay <benjlindsay@gmail.com>
2022-02-26 22:24:47 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+col_1, col_2, col_3 = st.columns(3)
+col_1.image("card_images/cards/2_of_clubs.png")
+col_2.image("card_images/cards/3_of_clubs.png")
+col_3.image("card_images/cards/4_of_clubs.png")
